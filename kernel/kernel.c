94218609a550ee5c5a5b3bcb8cc601bbe9a5283e
Mark <clashclanacc2602@gmail.com>
2019-10-24 22:30:27 +0300

    test

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -73,7 +73,7 @@ void kmain(multiboot_info_t* mbd, uint32_t magic) {
    init_timer(TIMER_FREQUENCY);
    logf("Timer initialized\n");
    __asm__ volatile("sti");
-   wait(10);
+   //wait(10);
    printf("this should appear after 10 sec");
    while(1) {
       
