197c3000247ff0a028472ba23c60bb64c7861eaf
Mark <clashclanacc2602@gmail.com>
2019-10-26 11:10:20 +0300

    fix

--- kernel/shellhandler.c
+++ kernel/shellhandler.c
@@ -1,14 +1,19 @@
 #include <frame_buffer.h>
 #include <string.h>
+#include <logger.h>
 
 char lastcommbuf[FB_COLS] = "";
 char terminal_buf[FB_CELLS] = "";
 
 void fb_newlinehandler(){
     char* command = lastcommbuf;
+    logf(lastcommbuf);
+    logf(terminal_buf);
+    logf(inttostr(strcmp(command, "work")));
     if (strcmp(command, "work") == 0){
         printf("YAAAAAAAAY\n\n\n\n\n\n\n");
     }
+    fb_newline();
 }
 
 void charbridge(char c){
