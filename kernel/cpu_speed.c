cb2cb79eafd410e6cbffb0b76cd251e9a15aa198
Mark <clashclanacc2602@gmail.com>
2019-10-27 11:17:51 +0200

    increase precision

--- kernel/cpu_speed.c
+++ kernel/cpu_speed.c
@@ -4,10 +4,10 @@
 int discoverspeed(){
     uint32_t hsec = getmsecfromboot();
     uint32_t clock = 0;
-    while(getmsecfromboot() - hsec != 10){
+    while(getmsecfromboot() - hsec != 1000){
         //15 cycles per each
         cpu_cycle();
         clock++;
     }
-    return (clock * 15) / 100000;
+    return (clock * 15) / 10000000;
 }
