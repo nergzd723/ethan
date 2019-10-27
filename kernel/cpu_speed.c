b3db18441ec241a7fd43dd0c7cd61bd7779a7134
Mark <clashclanacc2602@gmail.com>
2019-10-27 11:19:20 +0200

    dunno

--- kernel/cpu_speed.c
+++ kernel/cpu_speed.c
@@ -4,10 +4,10 @@
 int discoverspeed(){
     uint32_t hsec = getmsecfromboot();
     uint32_t clock = 0;
-    while(getmsecfromboot() - hsec != 1000){
+    while(getmsecfromboot() - hsec != 10){
         //15 cycles per each
         cpu_cycle();
         clock++;
     }
-    return (clock * 15) / 10000000;
+    return (clock * 60) / 100000;
 }
