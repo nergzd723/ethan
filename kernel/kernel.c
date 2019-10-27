03b8982da25a77a70a2bbd939cf5dca9b12aaff7
Mark <clashclanacc2602@gmail.com>
2019-10-27 10:40:40 +0200

    fix

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -83,7 +83,7 @@ void kmain(multiboot_info_t* mbd, uint32_t magic) {
 #endif
    __asm__ volatile("sti");
 #ifdef STANDALONE
-   printf("Ethan booted! Got ")
+   printf("Ethan booted! Got ");
    printf(inttostr(memorycount()));
    printf(" K mem");
    printf(">>> ");
