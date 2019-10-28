75bd96d77731230e14d0004039904651d7837e1d
Mark <clashclanacc2602@gmail.com>
2019-10-28 15:22:41 +0200

    add robin scheduler

--- kernel/includes/process.h
+++ kernel/includes/process.h
@@ -5,13 +5,13 @@
 
 #define MAX_PROCESS_NAME_LENGTH 20
 
-typedef struct {
+typedef struct _process_t {
     uint32_t pid;
     char task_name[MAX_PROCESS_NAME_LENGTH];
 
     uint32_t* kernel_stack_top;
     uint32_t page_directory;
-    uint32_t* next_task;
+    struct _process_t* next_task;
     uint32_t state;
 } process_t;
 
