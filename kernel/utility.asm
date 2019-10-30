ccc70485daa601d5e08ae46dde271532b8022b52
Mark <clashclanacc2602@gmail.com>
2019-10-30 17:10:41 +0200

    cant disable paging?

--- kernel/utility.asm
+++ kernel/utility.asm
@@ -29,11 +29,11 @@ load_page_directory:
    mov eax, [esp+4]
    mov cr3, eax
    ret
-   
+
 global disable_paging
 disable_paging:
    mov eax, cr0
-   and eax, 0x7FFFFFFe
+   and eax, 0x80000000
    mov cr0, eax
    ret
 
