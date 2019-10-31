c2227572c5a098d50235450b4857909e7f809828
Mark <clashclanacc2602@gmail.com>
2019-10-31 16:47:26 +0200

    aaaa

--- kernel/includes/timer.h
+++ kernel/includes/timer.h
@@ -4,12 +4,11 @@
 #include <stdint.h>
 
 #define TIMER_FREQUENCY 1000
-#define MILLISECOND 1000
+#define MILLISECOND 10
 
 void init_timer(uint32_t frequency);
 int getsecfromboot();
 void wait(int second);
 void waitm(int msec);
 uint32_t getmsecfromboot();
-void sleep(uint32_t microsecond);
 #endif
