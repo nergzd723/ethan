421db20c5a51ddaa4441c0621040ca53748483b6
Mark <clashclanacc2602@gmail.com>
2019-10-31 16:53:34 +0200

    add 150

--- kernel/includes/sound.h
+++ kernel/includes/sound.h
@@ -1,9 +1,9 @@
 #define C6 1000
-#define D6 1175
+#define D6 1150
 #define E6 1300
-#define F6 1400
-#define G6 1550
-#define A6 1760
-#define B6 1975
+#define F6 1450
+#define G6 1600
+#define A6 1750
+#define B6 1900
 void beep();
 void happy();
