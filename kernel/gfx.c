4b4a0d143436bd6289c9806b09cc58dde9b0fafd
Mark <clashclanacc2602@gmail.com>
2019-10-31 22:43:50 +0200

    here we go, debugging world!

--- kernel/gfx.c
+++ kernel/gfx.c
@@ -12,16 +12,16 @@ void gfx_test()
      
     // switch to 320x200x256 graphics mode
     regs.ax = 0x0013;
-    int32(0x10, &regs);
-     
-    // full screen with blue color (1)
-    memset((char *)0xA0000, 1, (320*200));
+//    int32(0x10, &regs);
+//     
+//    // full screen with blue color (1)
+//    memset((char *)0xA0000, 1, (320*200));
      
     // draw horizontal line from 100,80 to 100,240 in multiple colors
-    for(y = 0; y < 200; y++)
-        memset((char *)0xA0000 + (y*320+80), y, 160);
-    wait(3);
-    regs.ax = 0x0003;
-    int32(0x10, &regs);
-    clear_screen();
+//    for(y = 0; y < 200; y++)
+//        memset((char *)0xA0000 + (y*320+80), y, 160);
+//    wait(3);
+//    regs.ax = 0x0003;
+//    int32(0x10, &regs);
+//    clear_screen();
 }
