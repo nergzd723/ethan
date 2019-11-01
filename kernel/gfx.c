3bf2641dc8402bc857715bfd1590eeb8e3f15491
Mark <clashclanacc2602@gmail.com>
2019-11-01 13:01:32 +0200

    hmm

--- kernel/gfx.c
+++ kernel/gfx.c
@@ -1,27 +1,20 @@
-#include <rmonitor.h>
 #include <string.h>
 #include <timer.h>
 #include <frame_buffer.h>
+#include <bios32.h>
+
+void vesa_set_mode(uint32_t mode) {
+    register16_t reg_in = {0};
+    register16_t reg_out = {0};
+    reg_in.ax = 0x4F02;
+    reg_in.bx = mode;
+    bios32_service(BIOS_GRAPHICS_SERVICE, &reg_in, &reg_out);
+}
 
 //test
 void gfx_test()
 {
-//    asm volatile("sti");
-//    int y;
-//    regs16_t regs;
-     
-    // switch to 320x200x256 graphics mode
-//    regs.ax = 0x0013;
-//    int32(0x10, &regs);
-//     
-//    // full screen with blue color (1)
-//    memset((char *)0xA0000, 1, (320*200));
-     
-    // draw horizontal line from 100,80 to 100,240 in multiple colors
-//    for(y = 0; y < 200; y++)
-//        memset((char *)0xA0000 + (y*320+80), y, 160);
-//    wait(3);
-//    regs.ax = 0x0003;
-//    int32(0x10, &regs);
-//    clear_screen();
+    asm volatile("sti");
+    vesa_set_mode(0x144 | 0x4000);
+    return;
 }
