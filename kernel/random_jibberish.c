954f67710f2f68b474a114a4e2450b34067f3374
Mark <clashclanacc2602@gmail.com>
2019-11-01 19:55:52 +0200

    why isnt it random?

--- kernel/random_jibberish.c
+++ kernel/random_jibberish.c
@@ -6,6 +6,6 @@ void dojibberish(){
     asm volatile("sti");
     clear_screen();
     while(1){
-        fb_write_cell(rand()%FB_CELLS, inttostr(rand()%128), FB_GREEN, FB_BLACK);
+        fb_write_cell(rand()%FB_CELLS, (char)inttostr(rand()%128), FB_GREEN, FB_BLACK);
     }
 }
