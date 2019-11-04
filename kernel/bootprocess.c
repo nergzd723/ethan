9a248eaa859a6b18b39eabaa79e32c56dee9bc4b
Mark <clashclanacc2602@gmail.com>
2019-11-04 16:36:06 +0200

    add fixes

--- kernel/bootprocess.c
+++ kernel/bootprocess.c
@@ -75,6 +75,32 @@ void boot_stage1(){
    printf("Time-of-day clock...\n");
    rtc_install();
    printf("OK\n");
+   printf("Detecting SMBIOS...\n");
+   unsigned char *mem = (unsigned char *) 0xF0000;
+   int length, i;
+   unsigned char checksum;
+	while ((unsigned int) mem < 0x100000)
+	{
+		if (memcmp(mem,"_SM_",4)==0)
+		{
+			length = mem[5];
+			checksum = 0;
+			for(i = 0; i < length; i++)
+			{
+				checksum += mem[i];
+			}
+			if(checksum == 0) break;
+		}
+		mem += 16;
+	}
+	if ((unsigned int) mem == 0x100000)
+	{
+		logf("error: SMBIOS not found!");
+		
+	}
+	smb_ep = (struct SMBIOSEntryPoint*)mem;
+	logf("SMBIOS found at 0x%08x.");
+    printf("OK\n");
    printf("Entering second stage...\n");
    boot_stage2();
 }
