c7a92991d02cf412ae980f4009ef7807fa6d0d63
Mark <clashclanacc2602@gmail.com>
2019-11-04 22:00:14 +0200

    fix

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -1,8 +1,10 @@
 #include <isr.h>
+#include <logger.h>
+
+void syscall_handler(){
+    logf("Syscall!\n");
+}
 
 void install_syscall_handler(){
     register_interrupt_handler(149, &syscall_handler);
-}
-void syscall_handler(){
-    logf("Syscall!\n")
 }
