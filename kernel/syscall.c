a8ff2cbdf0d2f212ca754413c22cacee7dae4a0f
Mark <clashclanacc2602@gmail.com>
2019-12-07 20:02:23 +0200

    add sti

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -12,6 +12,7 @@ void syscall_handler(context_t* cpustate)
         logf("syscall: trying to breakthrough to shell!");
         clear_screen();
         printf("Process closed");
+        asm volatile ("sti");
         for(;;);
     case 0x001:
         printf("\nIndeed\n");
