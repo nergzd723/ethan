746ba2efec2bdeda8003e34b41382ad56af1cf94
Mark <clashclanacc2602@gmail.com>
2019-12-08 13:19:54 +0200

    MORE random

--- kernel/random_jibberish.c
+++ kernel/random_jibberish.c
@@ -5,12 +5,9 @@
 
 void dojibberish(){
     asm volatile("sti");
-    scall();
     clear_screen();
-    int a;
     while(1){
-        int a = rand();
-        fb_write_cell(a%FB_CELLS, rand(), FB_GREEN, FB_BLACK);
-        move_cursor_to_pos(a%FB_CELLS);
+        fb_write_cell(rand()%FB_CELLS, rand(), FB_GREEN, FB_BLACK);
+        move_cursor_to_pos(rand()%FB_CELLS);
     }
 }
