360ba50f3f47a63cfe8cbf4baa50f79af0105701
Mark <clashclanacc2602@gmail.com>
2019-12-10 19:19:04 +0200

    lets dont mess with it

--- kernel/shellhandler.c
+++ kernel/shellhandler.c
@@ -111,7 +111,7 @@ void fb_newlinehandler(){
         reinit();
     }
     if (strcmp(command, "gfx") == 0){
-        write_regs(g_320x200x256);
+        write_regs(g_640x480x16);
         vmode();
         clear_screen_a();
     }
