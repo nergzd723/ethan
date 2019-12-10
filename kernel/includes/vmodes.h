abb045a675f2192809fc2abdc135387bdfd52c69
Mark <clashclanacc2602@gmail.com>
2019-12-10 19:10:52 +0200

    fpx

--- kernel/includes/vmodes.h
+++ kernel/includes/vmodes.h
@@ -1,5 +1,5 @@
-unsigned char g_90x60_text[];
-unsigned char g_80x25_text[];
-unsigned char g_90x30_text[];
-unsigned char g_320x200x256[];
-unsigned char g_640x480x16[];
+unsigned char g_90x60_text[61];
+unsigned char g_80x25_text[61];
+unsigned char g_90x30_text[61];
+unsigned char g_320x200x256[61];
+unsigned char g_640x480x16[61];
