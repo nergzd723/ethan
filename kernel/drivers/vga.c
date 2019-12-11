853cea52bdcb29e9b044715e78e224c1058fedbf
Mark <clashclanacc2602@gmail.com>
2019-12-11 20:32:59 +0200

    c

--- kernel/drivers/vga.c
+++ kernel/drivers/vga.c
@@ -796,12 +796,7 @@ void write_regs(unsigned char *regs, bool isVmode)
 		outb(VGA_AC_WRITE, *regs);
 		regs++;
 	}
-	if (isVmode){
-		//write_font(g_8x8_font, fbpm);
-	}
-	else{
-		write_font(font_16, (char*)0xB800);
-	}
+	write_font(font_16, (char*)0xB800);
 /* lock 16-color palette and unblank display */
 	(void)inb(VGA_INSTAT_READ);
 	outb(VGA_AC_INDEX, 0x20);
