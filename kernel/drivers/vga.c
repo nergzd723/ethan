597fbeb733491a17193efb5c240c51a347e3022f
Mark <clashclanacc2602@gmail.com>
2019-12-12 18:50:43 +0200

    i completely fucked myself up...

--- kernel/drivers/vga.c
+++ kernel/drivers/vga.c
@@ -756,7 +756,7 @@ int i, j;
 void write_regs(unsigned char *regs, bool isVmode)
 {
 	unsigned i;
-	write_font(font_16, (char*)0xB800);
+
 /* write MISCELLANEOUS reg */
 	outb(VGA_MISC_WRITE, *regs);
 	regs++;
@@ -797,6 +797,7 @@ void write_regs(unsigned char *regs, bool isVmode)
 		outb(VGA_AC_WRITE, *regs);
 		regs++;
 	}
+	if(isVmode){write_font(font_16, (char*)0xB800);}
 /* lock 16-color palette and unblank display */
 	(void)inb(VGA_INSTAT_READ);
 	outb(VGA_AC_INDEX, 0x20);
