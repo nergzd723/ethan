da7bfca30a5bb85df1391f01baca6fdc398fea78
Mark <right2602@gmail.com>
2020-01-26 10:48:04 +0200

    fix to running state

--- kernel/boot/boot.asm
+++ kernel/boot/boot.asm
@@ -33,19 +33,10 @@ global start
 extern kmain
 start:
     cld                             ; Clear the direction flag for string operations
-    mov esp, eax ; save eax
-    lgdt[GDT_V] ; set up hardcoded GDT
-    mov ax, 8
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    mov ss, ax
-    mov eax, esp ; restore it
     mov esp, kernel_stack_top       ; Set up the stack
     push eax;                       ; Push multiboot header
     push ebx;                       ; Push multiboot magic
-    call 0x10:kmain                 ; Far call to kmain (never to return) sets up CS as well
+    call kmain                 ; Far call to kmain (never to return) sets up CS as well
 
 align 16
 GDT_T: 
